e8fef68199b26bc956771734ebdfbae7512e3999
Hyun-je <bigae2@gmail.com>
2024-05-28 00:45:58 +0900

    update model downloader

--- voice/model_downloader.sh
+++ voice/model_downloader.sh
@@ -1,2 +1,15 @@
-wget "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/$1/$2/$3/$1-$2-$3.onnx"
-wget "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/$1/$2/$3/$1-$2-$3.onnx.json"
+BASE_URL="https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/$1/$2/$3"
+ONNX_FILE="$1-$2-$3.onnx"
+JSON_FILE="$1-$2-$3.onnx.json"
+
+if [ -e $ONNX_FILE ] ; then
+    echo "$ONNX_FILE file exist"
+else
+    wget $BASE_URL/$ONNX_FILE
+fi
+
+if [ -e $JSON_FILE ] ; then
+    echo "$JSON_FILE file exist"
+else
+    wget $BASE_URL/$JSON_FILE
+fi
