ec2c05b96ea3c1371c75bff85b0c4caef0ee9fb9
Hyun-je <bigae2@gmail.com>
2024-05-28 00:32:53 +0900

    add model_downloader

---
+++ voice/model_downloader.sh
@@ -0,0 +1,2 @@
+wget "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/$1/$2/$3/$1-$2-$3.onnx"
+wget "https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/$1/$2/$3/$1-$2-$3.onnx.json"
