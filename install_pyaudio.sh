94c8607b1b867abdfff9078165241316547de37a
Hyun-je <bigae2@gmail.com>
2024-05-27 23:55:28 +0900

    add pyaudio installation script for raspberry pi

---
+++ install_pyaudio.sh
@@ -0,0 +1,12 @@
+sudo apt-get remove libportaudio2
+sudo apt-get install libasound2-dev
+
+git clone -b alsapatch https://github.com/gglockner/portaudio
+cd portaudio
+./configure && make
+sudo make install
+sudo ldconfig
+cd ..
+rm -rf portaudio
+
+sudo pip install pyaudio
