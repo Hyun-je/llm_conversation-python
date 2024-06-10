2129821dbaece7d3d40400f829f6c3c6aea9ac57
Hyun-je <bigae2@gmail.com>
2024-06-10 21:54:55 +0900

    Update install_ollama.sh to fix pip installation command

--- install_ollama.sh
+++ install_ollama.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt install curl
 
 curl -fsSL https://ollama.com/install.sh | sh
-pip install ollama --break-system-packagesollama
+pip install ollama --break-system-packages
 
 ollama --version
