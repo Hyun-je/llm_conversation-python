131c389eb8ca5a5d12bfff9f89d58696c6b7a2ad
Hyun-je <bigae2@gmail.com>
2024-06-10 23:28:43 +0900

    chore: Remove inactive devices from UDPManager's device list

--- network/UDPManager.py
+++ network/UDPManager.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from UDPSender import UDPSender
 from UDPListener import UDPListener
 
@@ -30,11 +31,24 @@ class UDPManager:
 
     def on_received_device_ping(self, data, addr):
         if data['uuid'] not in self.device_list:
+            self.device_list[data['uuid']] = {
+                'hostname': data['hostname'],
+                'addr': addr,
+                'last_seen': time.time()
+            }
             print(f"New device found: {data['uuid']} {addr=}")
-        self.device_list[data['uuid']] = addr
+        else:
+            self.device_list[data['uuid']]['last_seen'] = time.time()
 
     def on_received_unknown(self, data, addr):
         print(f"Warning! Received unknown message: {data} {addr=}")
+
+    def remove_inactive_devices(self, timeout=10):
+        current_time = time.time()
+        for uuid, device in self.device_list.items():
+            if current_time - device['last_seen'] > timeout:
+                print(f"Device removed: {uuid}")
+                del self.device_list[uuid]
 
     async def send_ping(self):
         while True:
@@ -42,6 +56,7 @@ class UDPManager:
             message_type = 'device_ping'
             content = {}
             self.sender.send_dict(message_type, content)
+            self.remove_inactive_devices()
 
     async def run(self):
         await asyncio.gather(
